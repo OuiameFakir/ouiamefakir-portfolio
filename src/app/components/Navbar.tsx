96eff3f2a39342d1cb1652da20affcda93a3fbf8
Ouiame EL FAKIR <ouiamefakir@gmail.com>
2024-03-29 13:48:09 +0000

    Add navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import { Socials } from '../constants'
-import Image from "next/image";
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Navbar() {
-  return   <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid d-flex justify-content-between align-items-center">
-            <a className="navbar-brand" href="#">
-                WebDEV
-            </a>
-            <div  className="d-flex gap-2">
-                {Socials.map((social) => (
-                    <Image
-                    key={social.name}
-                    src={social.src}
-                    alt={social.name}
-                    width={28}
-                    height={28}
-                    />
-                ))}
-            </div>
-        </div>
+  return (
+    <nav className="navbar navbar-dark bg-dark p-3 sticky-top">
+      <div className="container-fluid">
+        <a className="navbar-brand fw-bold">
+          <span className="text-warning">O</span>EF :)
+        </a>
+        <form className="d-flex">
+          <a className="text-light mr-6 text-decoration-none" href="#home">
+            Homme
+          </a>
+          <a className="text-light mr-6 text-decoration-none" href="#about">
+            About
+          </a>
+          <a className="text-light mr-6 text-decoration-none" href="#projects">
+            Projects
+          </a>
+          <a className="text-light mr-6 text-decoration-none" href="#resume">
+            Resume
+          </a>
+          <a className="text-light mr-6 text-decoration-none" href="#contact">
+            Contact Me
+          </a>
+        </form>
+      </div>
     </nav>
- 
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,32 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/utils/theme';
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'Ouiame EL FAKIR Portfolio',
-}
+  title: "Ouiame EL FAKIR Portfolio | Personal Portfolio",
+  description: "Ouiame EL FAKIR is a Frontend Developer ",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+      <body className={inter.className}>
+        {/* <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
-              <CssBaseline />
-              {children}
-            </ThemeProvider>
-        </AppRouterCacheProvider>
+        <CssBaseline /> */}
+        {children}
+        {/* </ThemeProvider>
+        </AppRouterCacheProvider> */}
       </body>
     </html>
-  )
+  );
 }
